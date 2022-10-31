8457e4edd0034a240a61bc6419aa230895caeaae
andriGo <afomin129@gmail.com>
2022-10-31 19:38:53 +0200

    mission accomplished №3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function compoundInterest(startDeposit, annualRate, annualAmount, interestPeriod) {
-	return (startDeposit * (1 + annualRate / annualAmount) ** (annualAmount * interestPeriod));
+	return startDeposit * (1 + annualRate / annualAmount) ** (annualAmount * interestPeriod);
 }
 module.exports = compoundInterest;
